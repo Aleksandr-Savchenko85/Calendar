0033b42ac097abb7ed08d25926558e5f91af3ea6
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-08-21 17:24:37 +0300

    commit

--- lesson8/hw4/index.js
+++ lesson8/hw4/index.js
@@ -1,36 +1,31 @@
-const userData = {
-  name: 'Boby',
+const user = {
+  name: "Sam",
 };
 
-const addPropertyV1 = (userData, userld) => {
-  userData.id = userld;
-  return userData;
-
+const addPropertyV1 = (obj, key, value) => {
+  obj[key] = value;
+  return obj;
 };
 
-const addPropertyV2 = (userData, userld) => {
-  const key = { id: userld };
-  return Object.assign(userData, key)
-
-};
-
-const addPropertyV3 = (userData, userld) => {
-  const key = { id: userld };
-  let userData_copy = Object.assign({}, userData, key)
-  return userData_copy;
-
-};
-
-const addPropertyV4 = (userData, userld) => {
-  let userData_copy = { id: userld };
-  return Object.assign({ ...userData }, userData_copy)
+const addPropertyV2 = (obj, key, value) => {
+  const keys = { [key]: value };
+  return Object.assign(obj, keys)
 }
-const result1 = addPropertyV1(userData, '555');
-const result2 = addPropertyV2(userData, '555');
-const result3 = addPropertyV3(userData, '555');
-const result4 = addPropertyV4(userData, '555');
 
-console.log(result1);
-console.log(result2);
-console.log(result3);
-console.log(result4);
+const addPropertyV3 = (obj, key, value) => {
+  const keys = { [key]: value };
+  let user_copy = Object.assign({}, obj, keys)
+  return user_copy;
+
+};
+
+const addPropertyV4 = (obj, key, value) => {
+  let user_copy = { [key]: value };
+  return Object.assign({ ...obj }, user_copy)
+}
+
+console.log(addPropertyV1(user, 'age', 25));
+console.log(addPropertyV2(user, 'age', 25));
+console.log(addPropertyV3(user, 'age', 25));
+console.log(addPropertyV4(user, 'age', 25));
+
--- lesson8/hw5/index.js
+++ lesson8/hw5/index.js
@@ -1,24 +1,18 @@
-let user ={
-    name: "Tom",
-    age: 25,
-    height: 170,
+let user = {
+    'Jon Doe': 19,
+    'Tom': 17,
+    'Bob': 18,
+}
+//input =>obj;
+//output =>arr;
+
+function getAdults(obj) {
+    let arr = Object.values(obj);
+    let arr1 = [];
+
+    return
 }
 
-let user1 = Object.assign(user);
-let user2 = {...user}
-let user3 = {}; 
-for (const key in user) {
-     {
-        user3[key] = user[key]
-    }
-}
-user1.number = 15
-user2.hhh = 55
-user3.ggg =100
 
 
-
-console.log(user)
-console.log(user1)
-console.log(user2)
-console.log(user3)
+console.log(getAdults(user))
