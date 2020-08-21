6d2c4bd6b1a1eb2b065b7ea465a2dffd8dcc9e9d
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-08-21 14:22:27 +0300

    commit

--- lesson8/hw4/index.js
+++ lesson8/hw4/index.js
@@ -1,35 +1,35 @@
-const obj = {
+const userData = {
   name: 'Sam',
 };
 
-const addPropertyV1 = (obj, property) => {
-  obj.id = property;
-  return obj;
+const adduserIdV1 = (userData, userId) => {
+  userData.id = userId;
+  return userData;
 
 };
 
-const addPropertyV2 = (obj, property) => {
-  const key = { id: property };
-  return Object.assign(obj, key)
+const adduserIdV2 = (userData, userId) => {
+  const key = { id: userId };
+  return Object.assign(userData, key)
 
 };
 
-const addPropertyV3 = (obj, property) => {
-  const key = { id: property };
-  let obj_copy = Object.assign({}, obj, key)
-  return obj_copy;
+const adduserIdV3 = (userData, userId) => {
+  const key = { id: userId };
+  let userData_copy = Object.assign({}, userData, key)
+  return userData_copy;
 
 };
 
-const addPropertyV4 = (obj, property) => {
-  let obj_copy = { id: property };
-  return Object.assign({ ...obj }, obj_copy)
+const adduserIdV4 = (userData, userId) => {
+  let userData_copy = { id: userId };
+  return Object.assign({ ...userData }, userData_copy)
 
 };
-/* const result1 = addPropertyV1(obj, '555555');
-const result2 = addPropertyV2(obj, '555555');
-const result3 = addPropertyV3(obj, '555555');
-const result4 = addPropertyV4(obj, '555555');
+/* const result1 = adduserIdV1(userData, '555555');
+const result2 = adduserIdV2(userData, '555555');
+const result3 = adduserIdV3(userData, '555555');
+const result4 = adduserIdV4(userData, '555555');
 
 console.log(result1);
 console.log(result2);
