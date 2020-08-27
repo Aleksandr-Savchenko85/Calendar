21fab5e8e42ef11e14d8912817d20772f19eec54
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-08-27 13:16:19 +0300

    commit

--- project/src/Header.jsx
+++ project/src/Header.jsx
@@ -4,18 +4,19 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="header">
-        <button className="button create-event-btn"><i className="fas fa-plus create-event-btn__icon"></i>Create</button>
-        <div className="navigation">
-            <button data-direction="today" className="navigation__today-btn button">Today</button>
-            <button data-direction="prev" className="icon-button navigation__nav-icon">
-                <i className="fas fa-chevron-left"></i>
-            </button>
-            <button data-direction="next" className="icon-button navigation__nav-icon">
-                <i className="fas fa-chevron-right"></i>
-            </button>
-            <span className="navigation__displayed-month">Jun - Jul 2019</span>
-        </div>
-    </header>
+            <button className="button create-event-btn"><i className="fas fa-plus create-event-btn__icon"></i>Create</button>
+            <div className="navigation">
+                <button data-direction="today" className="header_button-today">Today</button>
+                <button data-direction="prev" className="icon-button navigation__nav-icon">
+                    <i className="fas fa-chevron-left"></i>
+                </button>
+                <button data-direction="next" className="icon-button navigation__nav-icon">
+                    <i className="fas fa-chevron-right"></i>
+                </button>
+                <span className="navigation__displayed-month">
+                     August 2020</span>
+            </div>
+        </header>
     )
 };
 
