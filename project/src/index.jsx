9a5a6b81b950f76ed50a20aebf79f0844bd92d52
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-08-28 15:44:21 +0300

    commit

--- project/src/index.jsx
+++ project/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App.jsx';
-import Header from './Header.jsx'
+
 
 
 const rootElement = document.querySelector('#root');
