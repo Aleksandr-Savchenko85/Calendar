e091132605e7448e28d5b33dbb407c80410f3645
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-09-11 21:58:42 +0300

    commit

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import MainBoard from './main/MainBoard';
-import HeaderMain from './header/HeaderMain'
+import HeaderMain from './header/HeaderMain';
+import Week from './main/Week';
+import moment from 'moment';
 
-const App = ()=>{
-  return(
-    <div className="calendar">
-      <HeaderMain/>
-      <MainBoard/>
-    </div>
-    
-    
-  )
-}
+class App extends React.Component {
+  state = {
+    currentDate: moment(),
+
+  }
+
+  
+
+  render() {
+    return (
+      <div className="calendar">
+        <HeaderMain />
+        <MainBoard />
+      </div>
+
+
+    )
+  }
+
+
+};
 
 export default App;
+
+
+
+
--- project/src/header/ButtonsNavigation.jsx
+++ project/src/header/ButtonsNavigation.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import {goPrev, goNext} from '../App'
 
 
 const ButtonsNavigation = () => {
     return (
         <>
-            <button className="prev-btn"><i className="fas fa-chevron-left"></i></button>
+            <button className="prev-btn" ><i className="fas fa-chevron-left"></i></button>
             <button className="next-btn"><i className="fas fa-chevron-right"></i></button>
         </>
     )
