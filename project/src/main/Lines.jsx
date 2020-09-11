8baf313278f1757cc2fc9172d4e0cd41d3d50676
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-09-11 21:05:59 +0300

    commit

--- project/src/main/Lines.jsx
+++ project/src/main/Lines.jsx
@@ -2,32 +2,32 @@ import React from 'react'
 
 const Lines = () => {
     return (
-        <div class="lines">
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
-            <div class="lines__line"></div>
+        <div className="lines">
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
+            <div className="lines__line"></div>
 
         </div>
 
