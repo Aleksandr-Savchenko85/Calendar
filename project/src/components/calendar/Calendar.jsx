257fbc0b6416fbe4052b01597c8e1b05e87e4559
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-09-17 21:32:56 +0300

    commit

--- project/src/components/calendar/Calendar.jsx
+++ project/src/components/calendar/Calendar.jsx
@@ -1,14 +1,13 @@
-import React, {Component, useState} from 'react';
+import React, { Component, useState } from 'react';
 
 import Navigation from './../navigation/Navigation';
 import Week from '../week/Week';
 import Sidebar from '../sidebar/Sidebar';
 import events from '../../gateway/events';
-
 import './calendar.scss';
 
 
- class Calendar extends Component {
+class Calendar extends Component {
 
     state = {
         events,
@@ -29,6 +28,28 @@ import './calendar.scss';
             </section>
         )
     }
-} 
+}
 
 export default Calendar;
+/* 
+const Calendar = () => {
+    const [weekDates, setWeekDates] = useState(events)
+    return (
+
+        <section className="calendar">
+            <Navigation weekDates={weekDates} />
+            <div className="calendar__body">
+                <div className="calendar__week-container">
+                    <Sidebar />
+                    <Week weekDates={weekDates} events={events} />
+
+
+                </div>
+            </div>
+        </section>
+    )
+
+
+
+}
+export default Calendar; */
