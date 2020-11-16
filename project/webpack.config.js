a0eace928da34712df42aada30ffbc0724c70ed5
Aleksandr Savchenko <s1a9v8a5@ukr.net>
2020-11-16 15:18:44 +0200

    commit

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -5,54 +5,56 @@ const CopyPlugin = require("copy-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
-  const config = {
-    entry: "./src/index.jsx",
-    output: {
-      path: `${__dirname}/build`,
-      filename: "bundle.js",
-    },
-    module: {
-      rules: [
-        {
-          test: /.jsx?$/,
-          use: ["babel-loader"],
-        },
-        {
-          test: /.s?css$/,
-          use: [
-            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-            "css-loader",
-            "sass-loader",
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new webpack.ProgressPlugin(),
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-      }),
-      new CopyPlugin({
-        patterns: [{ from: "_redirects", to: "" }],
-      }),
-    ],
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
-    devServer: {
-      hot: true,
-    },
-  };
+    const isProduction = argv.mode === "production";
+    const config = {
+        entry: "./src/index.jsx",
+        output: {
+            filename: "bundle.js",
+            publicPath: '/',
+            path: __dirname + '/build'
 
-  if (isProduction) {
-    config.plugins.push(
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-      })
-    );
-  }
+        },
 
-  return config;
+        module: {
+            rules: [{
+                    test: /.jsx?$/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /.s?css$/,
+                    use: [
+                        isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+                        "css-loader",
+                        "sass-loader",
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new webpack.ProgressPlugin(),
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+            }),
+            new CopyPlugin({
+                patterns: [{ from: "_redirects", to: "" }],
+            }),
+        ],
+        resolve: {
+            extensions: [".js", ".jsx"],
+        },
+        devServer: {
+            hot: true,
+        },
+    };
+
+    if (isProduction) {
+        config.plugins.push(
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+            })
+        );
+    }
+
+    return config;
 };
